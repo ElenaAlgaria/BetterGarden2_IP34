41a3c268d6a35352f7fa7c6d6727ea6648dab68a
Kevin Schlatter <kevin.schlatter@students.fhnw.ch>
2021-11-02 16:48:02 +0100

    implement(vernetzungsprojekt): implement that textfields are not null/empty when button is clicked

--- lib/screens/vernetzungsprojekt/create_vernetzungsprojekt.dart
+++ lib/screens/vernetzungsprojekt/create_vernetzungsprojekt.dart
@@ -7,49 +7,68 @@ import 'package:biodiversity/components/drawer.dart';
 class CreateVernetzungsprojekt extends StatelessWidget {
   CreateVernetzungsprojekt({Key key}) : super(key: key);
 
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Vernetzungsprojekt erstellen')),
-      drawer: MyDrawer(),
-      body: Column(
-      children: [
-         const TextField(
-          decoration:InputDecoration(
-            hintText: 'Projekttitel',
-            labelText: 'Projekttitel',
-            labelStyle: TextStyle(
-              fontSize: 15,
-              color: Colors.black,
-/*              validaor: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Geben Sie Ihrem Vernetzungsprojekt einen Titel.';
-                }
-                return null;
-              }*/
-            )
-          ),
-              maxLength: 20,
-        ),
-        const TextField(
-          decoration: InputDecoration(
-            hintText: 'Projektbeschreibung',
-            labelText: 'Projektbeschreibung',
-            labelStyle: TextStyle(
-              fontSize: 15,
-              color: Colors.black,
-            )
-          ),
-            maxLength: 500,
-          maxLines: 2,
-        ),
-        ElevatedButton.icon(
-          onPressed: () {  },
-          label: Text("Vernetzungsprojekt speichern"),
-          icon: Icon(Icons.save) ,
-        )
-      ],
-    )
-    );
+        appBar: AppBar(title: const Text('Vernetzungsprojekt erstellen')),
+        drawer: MyDrawer(),
+        body: Builder(
+            builder: (context) => Center(
+                child: Form(
+                    key: _formkey,
+                    child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Geben Sie Ihrem Projekt einen Titel.';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: const InputDecoration(
+                                  hintText: 'Projekttitel',
+                                  labelText: 'Projekttitel',
+                                  labelStyle: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                  )),
+                              maxLength: 20,
+                            ),
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Geben Sie Ihrem Projekt eine Beschreibung.';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: const InputDecoration(
+                                  hintText: 'Projektbeschreibung',
+                                  labelText: 'Projektbeschreibung',
+                                  labelStyle: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                  )),
+                              maxLength: 500,
+                              maxLines: 2,
+                            ),
+                            ElevatedButton.icon(
+                              onPressed: () {
+                                if (!_formkey.currentState.validate()) {
+                                  return;
+                                }
+                              },
+                              label: Text("Vernetzungsprojekt speichern"),
+                              icon: Icon(Icons.save),
+                            )
+                          ],
+                        ))))));
   }
 }
