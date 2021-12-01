b456d648028befe90f75e834baae476cd68b9054
FlorianUngerGD <florian.unger@gemdat.ch>
2021-12-01 14:57:03 +0100

    fix(garden): deleted ownedLinkingProjects

--- lib/models/garden.dart
+++ lib/models/garden.dart
@@ -52,7 +52,6 @@ class Garden extends ChangeNotifier {
     gardenType = '';
     gardenType = '';
     ownedObjects = {};
-    ownedConnectionProjects = [];
     coordinates = const GeoPoint(0, 0);
     creationDate = DateTime.now();
     _isEmpty = true;
@@ -73,10 +72,6 @@ class Garden extends ChangeNotifier {
     }
   }
 
-  /// which Vernetzungsprojekte are contained in this garden
-  //TODO: Implement Vernetzungsprojekte and switch String to Vernetzungsprojekt
-  List<String> ownedConnectionProjects;
-
 
   /// creates a Garden from the provided Map.
   /// Used for database loading and testing
@@ -91,9 +86,6 @@ class Garden extends ChangeNotifier {
         ownedObjects = map.containsKey('ownedObjects')
             ? Map<String, int>.from(map['ownedObjects'] as Map)
             : {},
-        ownedConnectionProjects = map.containsKey('ownedLinkingProjects')
-            ? List<String>.from(map['ownedLinkingProjects'] as Iterable)
-            : [],
         coordinates = map.containsKey('coordinates')
             ? (map['coordinates'] as GeoPoint)
             : const GeoPoint(0, 0),
@@ -124,7 +116,6 @@ class Garden extends ChangeNotifier {
       'ownedObjects': ownedObjects,
       'coordinates': coordinates,
       'creationDate': creationDate,
-      'ownedLinkingProjects': ownedConnectionProjects,
       'imageURL': imageURL,
     });
   }
@@ -136,8 +127,6 @@ class Garden extends ChangeNotifier {
     owner = garden.owner;
     ownedObjects.clear();
     ownedObjects.addAll(garden.ownedObjects);
-    ownedConnectionProjects.clear();
-    ownedConnectionProjects.addAll(garden.ownedConnectionProjects);
     coordinates = garden.coordinates;
     gardenType = garden.gardenType;
     creationDate = garden.creationDate;
@@ -162,29 +151,10 @@ class Garden extends ChangeNotifier {
     }
   }
 
-  /// add a LinkingProject to the garden
-  /// and saves the garden to the database
-  void addLinkingProject(String linkingProject) {
-    if (linkingProject != null &&
-        linkingProject.isNotEmpty &&
-        !ownedConnectionProjects.contains(linkingProject)) {
-      ownedConnectionProjects.add(linkingProject);
-      saveGarden();
-    }
-  }
-
   /// removes a element from the garden, changes are saved automatically
   void removeFromOwnedObjects(String object) {
     if (ownedObjects.containsKey(object)) {
       ownedObjects.remove(object);
-      saveGarden();
-    }
-  }
-
-  /// removes a element from the garden, changes are saved automatically
-  void removeFromLinkingProjects(String object) {
-    if (ownedConnectionProjects.contains(object)) {
-      ownedConnectionProjects.remove(object);
       saveGarden();
     }
   }
