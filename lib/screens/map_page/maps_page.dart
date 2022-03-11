d9884c154f7902e8c761baddd086da8da09ab13a
Geremia Walzer <geremia.walzer@students.fhnw.ch>
2022-03-11 14:16:13 +0100

    Add Auto-Refresh on Maps Page. Newly created projects should instantly be shown. Still testing

--- lib/screens/map_page/maps_page.dart
+++ lib/screens/map_page/maps_page.dart
@@ -613,8 +613,8 @@ class _MapsPageState extends State<MapsPage> with TickerProviderStateMixin {
             heroTag: null,
             tooltip: 'Vernetzungsprojekt erstellen',
             backgroundColor: Theme.of(context).cardColor,
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              final value = await Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => CreateProjectPage(),
@@ -625,6 +625,9 @@ class _MapsPageState extends State<MapsPage> with TickerProviderStateMixin {
                   ),
                 ),
               );
+              setState(() {
+                initState();
+              });
             },
             child: Icon(icons[2], color: Theme.of(context).backgroundColor),
           ),
