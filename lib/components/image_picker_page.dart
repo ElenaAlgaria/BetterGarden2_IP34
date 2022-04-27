5f9c0efc12eff5ad0a08da21c014af71c334938f
LionelVallotton <lionel.vallotton@students.fhnw.ch>
2022-04-27 16:15:00 +0200

    upload of an image fixed

--- lib/components/image_picker_page.dart
+++ lib/components/image_picker_page.dart
@@ -133,7 +133,7 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
     final pickedFile =
         await _picker.pickImage(source: picker.ImageSource.camera);
     setState(() {
-      _imageFile = pickedFile.path as File;
+      _imageFile = File(pickedFile.path);
       _editMode = true;
       _deleteRequested = false;
     });
@@ -142,9 +142,9 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
   /// Lets the user choose an Image from their Gallery
   Future<void> _pickImageFromGallery() async {
     final pickedFile =
-        await _picker.pickImage(source: picker.ImageSource.gallery);
+         await _picker.pickImage(source: picker.ImageSource.gallery);
     setState(() {
-      _imageFile = pickedFile.path as File;
+      _imageFile = File(pickedFile.path);
       _editMode = true;
       _deleteRequested = false;
     });
