15323ec96acfb3f9e002ededacb2d4bb4b2e58b8
Geremia Walzer <geremia.walzer@students.fhnw.ch>
2022-05-10 10:41:51 +0200

    Re-Design Project-page

--- lib/screens/project_page/project_page.dart
+++ lib/screens/project_page/project_page.dart
@@ -2,6 +2,7 @@ import 'package:biodiversity/components/drawer.dart';
 import 'package:biodiversity/components/join_connection_project_popup_button.dart';
 import 'package:biodiversity/components/leave_connection_project_button.dart';
 import 'package:biodiversity/models/connection_project.dart';
+import 'package:biodiversity/screens/detailview_page/detailview_page_information_object.dart';
 import 'package:biodiversity/screens/project_page/edit_project_page.dart';
 import 'package:biodiversity/services/service_provider.dart';
 import 'package:flutter/material.dart';
@@ -36,77 +37,96 @@ class _ProjectPageState extends State<ProjectPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(project.title)),
-      drawer: MyDrawer(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                project.title,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 40)),
-              Container(
-                width: 200.0,
-                height: 60.0,
-                child: TextField(
-                  readOnly: true,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                      borderSide: const BorderSide(color: Colors.grey, width: 2.0),
+        appBar: AppBar(title: Text(project.title)),
+        drawer: MyDrawer(),
+        body: Container (
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ServiceProvider.instance.imageService.getImage(
+                    ServiceProvider.instance.speciesService.getSpeciesByReference(project.targetSpecies).name,
+                    "species",
                     ),
-                    hintText: ServiceProvider.instance.speciesService
-                        .getSpeciesByReference(project.targetSpecies)
-                        .name,
-                  ),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+                Text(
+                  project.title,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 30),
                 ),
-              ),
-              const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-              Text(
-                project.description,
-                style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
-              ),
-              const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-              widget.joinedProject
-                  ? leaveConnectionProjectButton(connectionProject: project)
-                  : joinConnectionProjectButton(connectionProject: project),
-              if(widget.joinedProject == true)
-              Padding(
-                padding: const EdgeInsets.only(left: 8, bottom: 15),
-                child: TextButton(
-                  onPressed: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              EditProjectPage(
-                                project: widget.project,
-                                onConnectionProjectChanged: (_currentConnectionProject) {
-                                  //updatedProject = _currentConnectionProject;
-                                },
-                              )),
-                    );
-                    setState(() {
-                      //project = updatedProject;
-                    });
-                  },
-                  child: const Text(
-                    'Bearbeiten',
-                    style: TextStyle(decoration: TextDecoration.underline),
-                  ),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+                Container(
+                    child: ElevatedButton.icon(
+                        style: const ButtonStyle(
+                          visualDensity: VisualDensity.compact,
+                        ),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailViewPageInformationObject(
+                                ServiceProvider.instance.speciesService
+                                    .getSpeciesByReference(
+                                        project.targetSpecies),
+                              ),
+                            ),
+                          );
+                        },
+                        label: Text(
+                          ServiceProvider.instance.speciesService
+                              .getSpeciesByReference(project.targetSpecies)
+                              .name,
+                        ),
+                        icon: const Icon(Icons.emoji_nature))),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+                const Text(
+                  "Projektbeschreibung",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 18),
                 ),
-              )
-            ],
+                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 10)),
+                Text(
+                  project.description,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.normal, fontSize: 18),
+                ),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    if (widget.joinedProject == true)
+                      ElevatedButton.icon(
+                        onPressed: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EditProjectPage(
+                                      project: widget.project,
+                                      onConnectionProjectChanged:
+                                          (_currentConnectionProject) {
+                                        //updatedProject = _currentConnectionProject;
+                                      },
+                                    )),
+                          );
+                          setState(() {
+                            //project = updatedProject;
+                          });
+                        },
+                        label: const Text('Bearbeiten'),
+                        icon: const Icon(Icons.edit),
+                      ),
+                    SizedBox(width: 20),
+                    widget.joinedProject
+                        ? leaveConnectionProjectButton(
+                            connectionProject: project)
+                        : joinConnectionProjectButton(
+                            connectionProject: project),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
