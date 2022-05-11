b42df159d114d6372e1c87ca3470b2c09359c807
Kevin Schlatter <kevin.schlatter@students.fhnw.ch>
2022-05-11 09:39:26 +0200

    delete(unused method): delete unused method that I created while implementing the feature

--- lib/services/image_service.dart
+++ lib/services/image_service.dart
@@ -263,11 +263,6 @@ class ImageService extends ChangeNotifier {
     return urls;
   }
 
-/*  Widget getCopyrightName(String name, String type) {
-    var copyrightName = copyrightInfo[name];
-    return Text((copyrightName));
-  }*/
-
   Map<String, String> get copyrightInfo => _copyrightInfo;
 
   Map<String, String> get caption => _caption;
